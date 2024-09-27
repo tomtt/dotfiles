4e0c4a60c36a8f5f12d9b221943f7e1fbebbdea0
Murilo Romeo <muriloh65@gmail.com>
2024-09-27 12:36:10 -0300

    fix: removes code highlight when using ruby-lsp

--- .config/nvim/lua/plugins/core.lua
+++ .config/nvim/lua/plugins/core.lua
@@ -30,6 +30,21 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       inlay_hints = { enabled = false },
+      servers = {
+        ruby_lsp = {
+          on_attach = function(client, _)
+            -- Disable semantic highlighting provided by ruby-lsp
+            if client.server_capabilities.semanticTokensProvider then
+              client.server_capabilities.semanticTokensProvider = nil
+            end
+
+            -- Disable document highlighting feature when put cursor on words
+            if client.server_capabilities.documentHighlightProvider then
+              client.server_capabilities.documentHighlightProvider = false
+            end
+          end,
+        },
+      },
     },
   },
 }
