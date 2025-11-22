e371810f6ee15c51ada31d785e1e39aeab17fd35
Tom ten Thij <tomtenthij@gmail.com>
2025-11-22 12:27:18 +0100

    Vim: Add plugin: switch

---
+++ lua/config/plugins/switch.lua
@@ -0,0 +1,26 @@
+return {
+  "AndrewRadev/switch.vim",
+  config = function()
+    vim.cmd([[
+    let g:switch_custom_definitions =
+    \ [
+    \ switch#Words(['y', 'n']),
+    \ switch#Words(['yes', 'no']),
+    \ switch#Words(['if', 'unless']),
+    \ switch#Words(['disable', 'enable']),
+    \ switch#Words(['assert_not', 'assert']),
+    \ switch#Words(['assert_not_includes', 'assert_includes']),
+    \ switch#Words(['assert_changes', 'assert_no_changes']),
+    \ switch#Words(['a', 'an']),
+    \ switch#Words(['is', 'are']),
+    \ switch#Words(['first', 'second', 'third', 'fourth', 'fifth', 'sixth']),
+    \ ['success', 'failure'],
+    \ ['succeeded', 'failed'],
+    \ switch#Words(['succeed', 'fail']),
+    \ switch#Words(['succeeds', 'fails']),
+    \ switch#Words(['with', 'without']),
+    \ switch#Words(['potential_customer', 'non_potential_customer'])
+    \ ]
+    ]])
+  end,
+}
