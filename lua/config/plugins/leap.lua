ae52dad890be1a40c37b32a1dac94f574440d7e2
Tom ten Thij <tom@gridline.nl>
2025-12-02 10:24:44 +0100

    Vim: Leap: Disable auto-jumping to the first match

--- lua/config/plugins/leap.lua
+++ lua/config/plugins/leap.lua
@@ -1,6 +1,8 @@
 return {
   "ggandor/leap.nvim",
   config = function()
+    require('leap').opts.safe_labels = ''
+
     vim.keymap.set({ "n", "x", "o" }, "s", "<Plug>(leap)")
     vim.keymap.set("n", "S", "<Plug>(leap-from-window)")
   end,
