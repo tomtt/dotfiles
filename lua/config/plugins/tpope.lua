0e9fde3140228f0cc50d04832519f79c9881f54d
Tom ten Thij <tom@gridline.nl>
2025-12-08 12:46:48 +0100

    Vim: rails: Add alternate or create function, map to <leader>rA

--- lua/config/plugins/tpope.lua
+++ lua/config/plugins/tpope.lua
@@ -3,7 +3,6 @@ return {
   { "tpope/vim-bundler" },
   -- { "tpope/vim-endwise" },
   { "tpope/vim-fugitive" },
-  { "tpope/vim-projectionist" },
   { "tpope/vim-rake" },
   { "tpope/vim-repeat" },
   { "tpope/vim-scriptease" },
