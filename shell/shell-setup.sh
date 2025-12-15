64dd1f4e80ac3c0b849c84527b8b3596b7dc634d
Tom ten Thij <tom@gridline.nl>
2025-12-15 16:30:11 +0100

    Shell: Place shell sourced files in shell/ where they are not linked in root

--- shell/shell-setup.sh
+++ shell/shell-setup.sh
@@ -4,8 +4,8 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-eval "$(/opt/homebrew/bin/brew shellenv)"
 eval "$(rbenv init - --no-rehash zsh)"
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 # eval "$(/opt/homebrew/bin/brew shellenv)"
 FPATH=~/.rbenv/completions:"$FPATH"
 
