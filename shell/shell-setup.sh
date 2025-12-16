8bd180d1d0c54f30f84c7a720f7bcd232c5374ba
Tom ten Thij <tom@gridline.nl>
2025-12-16 10:09:43 +0100

    Zsh: Setup brew to work cross-platform

--- shell/shell-setup.sh
+++ shell/shell-setup.sh
@@ -4,9 +4,19 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
+# Setup homebrew from any of the likely locations it can reside
+for brew_bin in \
+    "/opt/homebrew/bin/brew" \
+    "/usr/local/bin/brew" \
+    "/home/linuxbrew/.linuxbrew/bin/brew"
+do
+    if [[ -x "$brew_bin" ]]; then
+        eval "$("$brew_bin" shellenv)"
+        break
+    fi
+done
+
 eval "$(rbenv init - --no-rehash zsh)"
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-# eval "$(/opt/homebrew/bin/brew shellenv)"
 FPATH=~/.rbenv/completions:"$FPATH"
 
 autoload -U compinit
