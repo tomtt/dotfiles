c453560b83c883b874d28175433183671f328813
Tom ten Thij <tomtenthij@gmail.com>
2025-12-21 12:32:45 +0100

    Ruby dotfiles

---
+++ .ruby_utils.rb
@@ -0,0 +1,4 @@
+$LOAD_PATH.unshift(File.expand_path('~/created/projects/ruby_utils/lib/phtml'))
+$LOAD_PATH.unshift(File.expand_path('~/created/projects/ruby_utils/lib/to_csv'))
+require 'phtml'
+require 'to_csv'
