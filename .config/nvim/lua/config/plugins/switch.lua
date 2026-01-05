a939cec885958dac2ffb1c1c4e0d6d014eab05f1
Tom ten Thij <tom@gridline.nl>
2026-01-05 16:24:10 +0100

    Vim: Add switch definitions

--- .config/nvim/lua/config/plugins/switch.lua
+++ .config/nvim/lua/config/plugins/switch.lua
@@ -14,12 +14,14 @@ return {
     \ switch#Words(['assert_difference', 'assert_no_difference']),
     \ switch#Words(['assert_not_equal', 'assert_equal']),
     \ switch#Words(['a', 'an']),
-    \ switch#Words(['is', 'are']),
     \ switch#Words(['first', 'second', 'third', 'fourth', 'fifth', 'sixth']),
+    \ switch#Words(['is', 'are']),
+    \ switch#Words(['left', 'right']),
     \ ['success', 'failure'],
     \ ['succeeded', 'failed'],
     \ switch#Words(['succeed', 'fail']),
     \ switch#Words(['succeeds', 'fails']),
+    \ switch#Words(['up', 'down']),
     \ switch#Words(['with', 'without']),
     \ switch#Words(['potential_customer', 'non_potential_customer'])
     \ ]
